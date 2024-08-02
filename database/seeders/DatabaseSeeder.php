444a4e7b42bf581090d180cab4519d523c7a5b4e
Carlos Colina <carlosecolina89@gmail.com>
2024-08-02 13:01:30 -0500

    tabla seede\r

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -41,7 +41,8 @@ class DatabaseSeeder extends Seeder
             SliderSeeder::class,
             SubCategorySeeder::class,
             ProductSeeder::class,
-            StatusSeeder::class
+            StatusSeeder::class,
+            PricesTableSeeder::class,
             //ServiceSeeder::class,
         ]);
     }
