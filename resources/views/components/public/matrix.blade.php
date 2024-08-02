cfeb9e62330dd657253d618e8415efeddc04f003
Carlos Colina <carlosecolina89@gmail.com>
2024-08-01 23:27:54 -0500

    visualizacion comobs

--- resources/views/components/public/matrix.blade.php
+++ resources/views/components/public/matrix.blade.php
@@ -50,6 +50,77 @@
 
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <script src="{{ asset('js/functions.js') }}?v={{ uniqid() }}"></script>
+  <style>
+    .select2-container .select2-selection--single {
+      margin-top: 0.25rem;
+      /* mt-1 */
+      background-color: #F9FAFB;
+      /* bg-gray-50 */
+      border: 1px solid #D1D5DB;
+      /* border-gray-300 */
+      color: #111827;
+      /* text-gray-900 */
+      border-radius: 0.5rem;
+      /* rounded-lg */
+      padding-left: 2.5rem;
+      /* pl-10 */
+      padding: 7px 14px;
+      /* p-2.5 */
+      height: unset;
+      font-size: 0.875rem;
+      /* text-sm */
+    }
+
+    .select2-container--open .select2-dropdown--below {
+      border-color: #D1D5DB;
+    }
+
+    .select2-container--default .select2-selection--single .select2-selection__rendered {
+      color: #111827;
+      /* text-gray-900 */
+    }
+
+    .select2-container--default .select2-selection--single .select2-selection__arrow {
+      height: 100%;
+      right: 1rem;
+      /* align with pl-10 */
+    }
+
+    .select2-container--default .select2-selection--single:focus {
+      border-color: #3B82F6;
+      /* focus:border-blue-500 */
+      box-shadow: 0 0 0 3px rgba(59, 130, 246, 0.5);
+      /* focus:ring-blue-500 */
+    }
+
+    /* Dark mode styles */
+    .dark .select2-container .select2-selection--single {
+      background-color: #374151;
+      /* dark:bg-gray-700 */
+      border-color: #4B5563;
+      /* dark:border-gray-600 */
+      color: #F9FAFB;
+      /* dark:text-white */
+    }
+
+    .dark .select2-container--default .select2-selection--single .select2-selection__rendered {
+      color: #F9FAFB;
+      /* dark:text-white */
+    }
+
+    .dark .select2-container--default .select2-selection--single:focus {
+      border-color: #3B82F6;
+      /* dark:focus:border-blue-500 */
+      box-shadow: 0 0 0 3px rgba(59, 130, 246, 0.5);
+      /* dark:focus:ring-blue-500 */
+    }
+
+    /* Adjust the width to match Tailwind CSS block w-full */
+    .select2-container {
+      width: 100% !important;
+      /* block w-full */
+    }
+  </style>
 
 </head>
 
