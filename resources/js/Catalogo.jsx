38d615fce9e43f0207beb8e5639febdd8879d0b4
Carlos Colina <carlosecolina89@gmail.com>
2024-08-21 13:14:15 -0500

    filtro megamenu

--- resources/js/Catalogo.jsx
+++ resources/js/Catalogo.jsx
@@ -16,6 +16,14 @@ import axios from 'axios'
 
 const Catalogo = ({ minPrice, maxPrice, categories, tags, attribute_values, id_cat: selected_category, tag_id, subCatId }) => {
   const take = 12
+  const [items, setItems] = useState([]);
+  const [filter, setFilter] = useState(selected_category ? { category_id: [selected_category] } : { 'txp.tag_id': [tag_id] });
+  const [totalCount, setTotalCount] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [showModal, setShowModal] = useState(false);
+  const is_proveedor = useRef(false);
+  const cancelTokenSource = useRef(null);
+
   useEffect(() => {
     const script = document.createElement('script');
     script.src = "js/notify.extend.min.js";
@@ -25,39 +33,33 @@ const Catalogo = ({ minPrice, maxPrice, categories, tags, attribute_values, id_c
     return () => {
       document.body.removeChild(script);
     };
-  }, [null]);
+  }, []);
 
-  const [items, setItems] = useState([])
-  const [filter, setFilter] = useState(selected_category ? { category_id: [selected_category] } : { 'txp.tag_id': [tag_id] })
-  const [totalCount, setTotalCount] = useState(0)
-  const [currentPage, setCurrentPage] = useState(1)
+  useEffect(() => {
+    setCurrentPage(1);
+    getItems();
+  }, [filter]);
 
-  const [showModal, setShowModal] = useState(false)
-  const is_proveedor = useRef(false);
-
-
+  useEffect(() => {
+    getItems();
+  }, [currentPage]);
 
   useEffect(() => {
     if (subCatId !== null) {
-      setFilter({ ...filter, subcategory_id: [subCatId] })
+      setFilter({ ...filter, subcategory_id: [subCatId] });
     }
-  }, [])
-
-  useEffect(() => {
-    setCurrentPage(1)
-    getItems()
-  }, [filter])
-
-  useEffect(() => {
-    getItems()
-  }, [currentPage])
-  let abortController = new AbortController();
+  }, []);
 
   const getItems = async () => {
-    abortController.abort('some');
-    abortController = new AbortController();
-    let signal = abortController.signal;
-    const filterBody = []
+    // Cancelar la solicitud anterior si existe
+    if (cancelTokenSource.current) {
+      cancelTokenSource.current.cancel('Operation canceled due to new request.');
+    }
+
+    // Crear un nuevo token de cancelación
+    cancelTokenSource.current = axios.CancelToken.source();
+
+    const filterBody = [];
 
     if (filter.maxPrice || filter.minPrice) {
       if (filter.maxPrice && filter.minPrice) {
@@ -73,106 +75,103 @@ const Catalogo = ({ minPrice, maxPrice, categories, tags, attribute_values, id_c
             'or',
             ['descuento', '<=', filter.maxPrice]
           ]
-        ])
+        ]);
       } else if (filter.minPrice) {
         filterBody.push([
           ['precio', '>=', filter.minPrice],
           'or',
           ['descuento', '>=', filter.minPrice]
-        ])
+        ]);
       } else if (filter.maxPrice) {
         filterBody.push([
           ['precio', '<=', filter.maxPrice],
           'or',
           ['descuento', '<=', filter.maxPrice]
-        ])
+        ]);
       }
     }
 
-
     if (filter['txp.tag_id'] && filter['txp.tag_id'].length > 0) {
-      const tagsFilter = []
+      const tagsFilter = [];
       filter['txp.tag_id'].forEach((x, i) => {
-        if (i == 0) {
-          tagsFilter.push(['txp.tag_id', '=', x])
+        if (i === 0) {
+          tagsFilter.push(['txp.tag_id', '=', x]);
         } else {
-          tagsFilter.push('or', ['txp.tag_id', '=', x])
+          tagsFilter.push('or', ['txp.tag_id', '=', x]);
         }
-      })
-      filterBody.push(tagsFilter)
+      });
+      filterBody.push(tagsFilter);
     }
 
     for (const key in filter) {
-      if (!key.startsWith('attribute-')) continue
-      if (filter[key].length == 0) continue
-      const [, attribute_id] = key.split('-')
-      const attributeFilter = []
+      if (!key.startsWith('attribute-')) continue;
+      if (filter[key].length === 0) continue;
+      const [, attribute_id] = key.split('-');
+      const attributeFilter = [];
       filter[key].forEach((x, i) => {
-        if (i == 0) {
-          attributeFilter.push(['apv.attribute_value_id', '=', x])
+        if (i === 0) {
+          attributeFilter.push(['apv.attribute_value_id', '=', x]);
         } else {
-          attributeFilter.push('or', ['apv.attribute_value_id', '=', x])
+          attributeFilter.push('or', ['apv.attribute_value_id', '=', x]);
         }
-      })
+      });
       filterBody.push([
         ['a.id', '=', attribute_id],
         'and',
         attributeFilter
-      ])
+      ]);
     }
 
     if (filter['category_id'] && filter['category_id'].length > 0) {
-      const categoryFilter = []
+      const categoryFilter = [];
       filter['category_id'].forEach((x, i) => {
-        if (i == 0) {
-          categoryFilter.push(['categoria_id', '=', x])
+        if (i === 0) {
+          categoryFilter.push(['categoria_id', '=', x]);
         } else {
-          categoryFilter.push('or', ['categoria_id', '=', x])
+          categoryFilter.push('or', ['categoria_id', '=', x]);
         }
-      })
-      filterBody.push(categoryFilter)
+      });
+      filterBody.push(categoryFilter);
     }
-
 
     if (filter['subcategory_id'] && filter['subcategory_id'].length > 0) {
-      const subcategoryFilter = []
+      const subcategoryFilter = [];
       filter['subcategory_id'].forEach((x, i) => {
-        if (i == 0) {
-          subcategoryFilter.push(['subcategory_id', '=', x])
+        if (i === 0) {
+          subcategoryFilter.push(['subcategory_id', '=', x]);
         } else {
-          subcategoryFilter.push('or', ['subcategory_id', '=', x])
+          subcategoryFilter.push('or', ['subcategory_id', '=', x]);
         }
-      })
-      filterBody.push(subcategoryFilter)
+      });
+      filterBody.push(subcategoryFilter);
     }
 
-    // const { status, result } = await Fetch('/api/products/paginate', {
-    //   method: 'POST',
-    //   body: JSON.stringify({
-    //     requireTotalCount: true,
-    //     filter: arrayJoin([...filterBody, ['products.visible', '=', true]], 'and'),
-    //     take,
-    //     skip: take * (currentPage - 1)
-    //   }),
+    try {
+      const { status, data: result } = await axios.post('/api/products/paginate', {
+        requireTotalCount: true,
+        filter: arrayJoin([...filterBody, ['products.visible', '=', true]], 'and'),
+        take,
+        skip: take * (currentPage - 1)
+      }, {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        cancelToken: cancelTokenSource.current.token
+      });
 
-    // })
-    const { status, data: result } = await axios.post('/api/products/paginate', {
-      requireTotalCount: true,
-      filter: arrayJoin([...filterBody, ['products.visible', '=', true]], 'and'),
-      take,
-      skip: take * (currentPage - 1)
-    }, {
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      signal: signal
-    });
+      is_proveedor.current = result?.is_proveedor ?? false;
 
-    is_proveedor.current = result?.is_proveedor ?? false
-
-    setItems(result?.data ?? [])
-    setTotalCount(result?.totalCount ?? 0)
-  }
+      setItems(result?.data ?? []);
+      setTotalCount(result?.totalCount ?? 0);
+    } catch (error) {
+      if (axios.isCancel(error)) {
+        console.log('Request canceled', error.message);
+      } else {
+        // Manejar otros errores
+        console.error(error);
+      }
+    }
+  };
 
   const attributes = attribute_values.reduce((acc, item) => {
     // If the attribute_id does not exist in the accumulator, create a new array for it
